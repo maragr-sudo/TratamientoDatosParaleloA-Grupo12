8328f49d87347ae7b603d31b68546ed80d25a276
maragr-sudo <marthi1901@gmail.com>
2025-08-18 00:46:11 -0500

    Primer commit de la API

---
+++ app.py
@@ -0,0 +1,29 @@
+"#Archivo con el codigo para la API" 
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify ({'message': 'Bienvenido a la API de Microservicio Base - Tratamiento de Datos Paralelo A'}) 
+
+@app.route('/api/sumar', methods=['POST'])
+def sumar():
+    data = request.get_json()
+    a =data.get('a')
+    b = data.get('b')
+    if  a is None or b is None:
+        return jsonify({'error': 'Faltan datos para sumar'}), 400
+    return jsonify({'resultado': a + b})
+@app.route('/api/info', methods=['GET'])
+def info():
+    return jsonify({
+        'nombre': 'Microservicio Base - Tratamiento de Datos Paralelo A',
+        'version': '1.0.0',
+        'descripcion': 'Este microservicio realiza operaciones básicas de suma.',
+        'autor': 'Martha Aguilar',
+    })    
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
